8fd1585810a6c4d392ce6bfac9c631142953940c
gaoyongsheng <gys270589787@163.com>
2021-01-14 17:49:09 +0800

    1.初始化

---
+++ shopping-micro-config/src/main/java/com/shopping/micro/config/ShoppingMicroConfigApplication.java
@@ -0,0 +1,15 @@
+package com.shopping.micro.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ShoppingMicroConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMicroConfigApplication.class, args);
+    }
+
+}
